8f740226c29a53032127d6d7f5ed72bf075b6e20
nuella001 <chiomamemmanuella@gmail.com>
2022-10-31 08:41:51 -0700

    added enough margin to the profile.jsx

--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -1,13 +1,22 @@
 import React from "react";
-import profile from './assets/images/profile.svg'
+import profile from "./assets/images/profile.svg";
+// import
 
 const Profile = () => {
   return (
     <div>
-      <div className="flex justify-center items-center ">
-        <img src={profile} alt="profile_image" />
+      <div className="relative cursor-pointer w-1/6 mx-auto mt-12 mb-12 ">
+        <div className="flex justify-center items-center   ">
+          <img className=" block" src={profile} alt="profile_image" />
+        </div>
+        {/* <div className="bg-black opacity-50  h-full absolute left-0 top-0  z-10
+        rounded-full w-24 right-0 bottom-0">
+          hello
+        </div> */}
       </div>
-      <h2  className="flex justify-center items-center"  >Billionaire Techgirle</h2>
+      <h2 className="flex justify-center items-center mb-8 font-bold text-xl">
+        Billionaire Techgirle
+      </h2>
     </div>
   );
 };
