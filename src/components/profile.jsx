beeeae9d3ddc98a57ac41ccc362c5cabc73b1792
nuella001 <chiomamemmanuella@gmail.com>
2022-10-31 09:09:13 -0700

    add the share icon

--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import profile from "./assets/images/profile.svg";
-// import
+import share from './assets/images/share.svg';
 
 const Profile = () => {
   return (
     <div>
+      <div className="flex justify-end w-4/6 absolute  ">
+        <img src={share} alt="" className="border-dotted border-blue-50  border-2 rounded-full p-4
+        cursor-pointer"/>
+      </div>
       <div className="relative cursor-pointer w-1/6 mx-auto mt-12 mb-12 ">
         <div className="flex justify-center items-center   ">
           <img className=" block" src={profile} alt="profile_image" />
